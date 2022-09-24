fed822be45aa056beabc841a18358814c2f0c62a
Spencer Boggs <92287368+Spinny2005@users.noreply.github.com>
2022-09-23 19:59:14 -0700

    Create run.bat

---
+++ Drive C/run.bat
@@ -0,0 +1,8 @@
+::  DRIVE C - FILE SCANNER
+::  run.bat - Scan all files on the Computer (Check if the Computer has a specific file)
+
+::  Disclaimer
+::  This flash drive is for personal use only, not to troll your friends.
+
+::  Needs to be updated
+dir /s /a-d c: > %CD%\ComputerFiles
