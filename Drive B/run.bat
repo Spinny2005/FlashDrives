110fd29d5e046b624cd8dd7623e29046a46d89de
Spencer Boggs <92287368+Spinny2005@users.noreply.github.com>
2022-09-23 20:00:51 -0700

    Update run.bat

--- Drive B/run.bat
+++ Drive B/run.bat
@@ -1,1 +1,8 @@
+::  DRIVE B - TROLL DRIVE
+::  run.bat - Creates a shit ton of folders on your desktop lol
+
+::  Disclaimer
+::  This flash drive is for personal use only, not to troll your friends.
+
+
 FOR /L %%I IN (1,1,20) DO md %userprofile%\OneDrive\desktop\TROLLED-LOL-%%I
