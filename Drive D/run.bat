13ec4de3db40087fdca3e028f4556e5d4f9c979e
Spencer Boggs <spencer_boggs@icloud.com>
2022-09-28 11:23:25 -0700

    Drive D

---
+++ Drive D/run.bat
@@ -0,0 +1,3 @@
+# Turn on telenet after running setup.bat
+echo ***If you are seeing this you need to run setup.bat and then rerun this file***
+telnet telehack.com
---
+++ Drive D/setup.bat
@@ -0,0 +1,3 @@
+echo on
+echo ***Make sure to enable telnet and then run run.bat***
+START C:\Windows\system32\OptionalFeatures.exe
